3bb5a6354fabbcb8a564d919544ca88094f70c64
anurag-si <anuragis4063@gmail.com>
2022-12-25 14:33:25 +0530

    Faceboo login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
+
+const responseFacebook = (response) => {
+  console.log(response);
+}
+
+const componentClicked = (data) => {
+  console.warn(data)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <FacebookLogin
+    appId="545460383955841"
+    autoLoad={true}
+    fields="name,email,picture"
+ 
+    callback={responseFacebook} />  
     </div>
   );
 }
